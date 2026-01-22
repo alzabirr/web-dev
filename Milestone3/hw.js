0d8c746d96659d99c5d923c72c33899d26dc7e48
alzabirr <alzabirhussain@gmail.com>
2026-01-23 04:43:54 +0600

    js start

---
+++ Milestone3/hw.js
@@ -0,0 +1,29 @@
+// Task 1 
+// let totalTk = 1000;
+// let kgOfApples = 350;
+// let kgOfOranges = 350;
+// let totalItems = kgOfApples + kgOfOranges
+
+// let totalcost = totalTk - totalItems
+// console.log(totalcost);
+
+// Task 2 
+// marks of the five subjects 
+// let mathematics = 75.25; 
+// let biology = 65;
+// let chemistry = 80;
+// let physics = 35.45;  
+// let bangla = 99.50;
+
+// let totalMarks = mathematics + biology + chemistry + physics + bangla;
+
+// let averageMarks = totalMarks /5
+// console.log( Number(averageMarks.toFixed(2)));
+
+
+
+// Task 3 
+// let givenNumber = 119 ;
+// console.log(givenNumber % 5);
+
+
