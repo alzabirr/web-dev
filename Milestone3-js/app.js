139f6c95b5c698e27dc90a51c5fbe94aed20d44c
alzabirr <alzabirhussain@gmail.com>
2026-01-25 04:34:30 +0600

    js condition

---
+++ Milestone3-js/app.js
@@ -0,0 +1,28 @@
+// // // let a = 10 
+// // // let b = 10 
+// // // let c = 10
+// // // let total = a+b+c 
+
+// // // console.log(total/3);
+
+// // const car = "red car ";
+
+
+// // console.log(typeof car)
+
+
+
+// // let a = 10 ;
+// // let b =  parseFloat( "20.5") ;
+// // console.log( a+b);
+// // // console.log(typeof b)
+ 
+
+// let a = 0.1 ;
+// let b = 0.2  ;
+// console.log(a+b.toFixed(4));
+// // console.log(typeof b)
+
+let x = 10;
+let num = Number(x);
+console.log(num);
---
+++ Milestone3-js/hw.js
@@ -0,0 +1,157 @@
+// // Task 1 
+// // let totalTk = 1000;
+// // let kgOfApples = 350;
+// // let kgOfOranges = 350;
+// // let totalItems = kgOfApples + kgOfOranges
+
+// // let totalcost = totalTk - totalItems
+// // console.log(totalcost);
+
+// // Task 2 
+// // marks of the five subjects 
+// // let mathematics = 75.25; 
+// // let biology = 65;
+// // let chemistry = 80;
+// // let physics = 35.45;  
+// // let bangla = 99.50;
+
+// // let totalMarks = mathematics + biology + chemistry + physics + bangla;
+
+// // let averageMarks = totalMarks /5
+// // console.log( Number(averageMarks.toFixed(2)));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // let year = 10 ;
+
+// // if(year >= 18 ){
+// //     console.log("you can now");
+    
+// // }
+// // else{
+// //     console.log("no")
+// // }
+
+
+
+// let result = 3.0;
+
+// if(result == 5.0){
+//     console.log("pc", result);
+    
+// }else if(result >= 4.5){
+//     console.log("tablet")
+// }else if (result >= 3.5){
+//     console.log("pen")
+
+// }else if (result >= 3){
+//     console.log("pencile",result)
+// }else{
+//     console.log("bari ajaw gumaw ")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let amrBoyos = 10 ; 
+let ENtryAge = 12 ;
+if(amrBoyos >= ENtryAge){
+    console.log("now jaw");
+    
+}else{
+    console.log("no no ")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
