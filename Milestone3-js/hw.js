65856052e8d12f1aa4508a61911e4b65284dd54d
alzabirr <alzabirhussain@gmail.com>
2026-01-25 05:20:51 +0600

    complex condition

--- Milestone3-js/hw.js
+++ Milestone3-js/hw.js
@@ -115,7 +115,7 @@ if(amrBoyos >= ENtryAge){
 
 
 
-
+  
 
 
 
---
+++ Milestone3-js/mumltipul_condition.js
@@ -0,0 +1,66 @@
+
+
+const salary = 25000;
+const isBcs = true;
+const height = 61;
+const house = 3 ;
+const car = true
+// 2 ta conditon true hawya lagbe 
+
+// if (salary > 20000 && height >= 60) {
+//     console.log("kobul");
+    
+// }else{
+//     console.log("hbe na bari ja ")
+// }
+
+
+
+// je kono 1 ba tar beshi  ta condition true hoilei hbe 
+
+// if(salary > 2600 || height >= 66    || house >2 ){
+//     console.log("kobul ");
+    
+// }
+// else{
+//     console.log("hbe na bari js");
+    
+// }
+
+
+
+// sob condition true hoite hbe 
+
+// if(salary > 2600 && height >= 66 && isBcs == true ){
+//     console.log("kobul ");
+    
+// }
+// else{
+//     console.log("hbe na bari js");
+    
+// }
+
+
+
+
+//  COMPLEX CONDITION 
+
+// if(  (salary > 29000 && house > 1) || height > 55){
+//     console.log("kobul");
+    
+// }
+// else{
+//     console.log("no no no ");
+    
+// }
+
+
+// complex condition 
+// if(  (salary > 29000 || house > 10) || height > 55){
+//     console.log("kobul");
+    
+// }
+// else{
+//     console.log("no no no ");
+    
+// }
