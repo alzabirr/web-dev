1a40bfaecd941516283d42d8fd64b4da49fd66fb
alzabirr <alzabirhussain@gmail.com>
2026-02-07 00:26:51 +0600

    logic build start

---
+++ Logic-build/function.js
@@ -0,0 +1,54 @@
+// function blenderMachine(fruit){
+//     console.log("ami fol theke juice banai ",fruit);
+    
+
+// }
+// // blenderMachine("mango")
+
+
+
+// // even and odd number 
+// // function findEvenNum(number1,number2 ){
+
+// //    let  sum = number1 + number2;
+// //     if(sum % 2 == 0){
+// //         console.log("even number",sum);
+        
+// //     }
+// //     else{
+// //         console.log("odd number ",sum);
+        
+// //     }
+
+// // }
+// // findEvenNum(2,8)
+// // findEvenNum(2,9)
+
+
+// function findEvenNumber(number){
+    
+//     if(number % 2 === 0){
+//         return " odd number "+ number
+        
+//     }else{
+//         // console.log("even number",number);
+//         return "even number " + number
+        
+//     }
+    
+
+// }
+// console.log(findEvenNumber(5));
+
+
+function add (a){
+    // console.log(a);
+    let sum = a ;
+    return sum;
+
+    
+
+}
+console.log(add(4));
+
+// add(3)
