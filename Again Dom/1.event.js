455c7250de6987f628cdc9ee73f0ec1f83f2b0ed
alzabirr <alzabirhussain@gmail.com>
2026-02-20 06:20:44 +0600

    learning  dom

---
+++ Again Dom/1.event.js
@@ -0,0 +1,13 @@
+ document.querySelector("#btn-update-title").addEventListener("click", function(){
+            const pageTitle = document.querySelector("#page-title");
+            pageTitle.innerText = "hi hi"
+        })
+
+
+
+        // loging btn 
+        document.querySelector("#log-in").addEventListener("click", function(){
+            const titleChange = document.querySelector("#log-title");
+            titleChange.innerText = "user login successfull"
+            
+        })
