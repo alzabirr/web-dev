e924dccf9c5f1f5d147c066ff8478118787fa80c
alzabirr <alzabirhussain@gmail.com>
2026-02-26 03:52:47 +0600

    es6

---
+++ Milestone-5-Es-6/1.let-conts.js
@@ -0,0 +1,31 @@
+// let price = 120;
+// price = 150;
+// console.log(price);
+
+
+// const dress = ["shirt", "pant"];
+// dress.pop("jama");
+// console.log(dress);
+
+// const er man cahneg kra jai na kintu assien kra jai 
+// const student = {
+//     name : "rafi",
+//     age : 22
+// }
+
+// student.name = "kafi"
+// console.log(student);
+
+
+let numbes = [-1,2,3,-3,5,-7,-8,-9,10];
+// for(let i = 0 ; i<=numbes.length; i++){
+//     const elm = numbes[i]
+//     if(elm >= 5){
+//         console.log("yes",elm);
+        
+//     }else{
+//         console.log("no",elm);
+        
+
+//     }
+// }
---
+++ Milestone-5-Es-6/2.default-para.js
@@ -0,0 +1,7 @@
+function add (num1, num2 = 0){
+    const total = num1 + num2;
+    console.log(num1, num2, "total is ", total);
+    
+
+}
+add(10,20)
