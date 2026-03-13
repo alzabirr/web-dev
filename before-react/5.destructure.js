3aec73f89b99a20420e1763a181f8df0df64a87f
alzabirr <alzabirhussain@gmail.com>
2026-03-13 14:10:52 +0600

    before dom

---
+++ before-react/5.destructure.js
@@ -0,0 +1,16 @@
+
+// Array 
+let arr  = [1,2,3,4,5,6,7,8];
+let [one, two, , , five]= arr;
+console.log(one,two,five);
+
+
+let obj = {
+    name : "Rafi",
+    address : "Alamdanga",
+    age : 22,
+    street : "1A001"
+
+}
+let {name, age, address}= obj
+console.log(age, name, address);
